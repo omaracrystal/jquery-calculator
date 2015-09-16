db2387cefd836d015cf0f74a028ad90ef4516d6c
omaracrystal <omaracrystal@gmail.com>
2015-09-16 15:41:01 -0600

    added functions to take numbers larger than 9

--- js/dom.js
+++ js/dom.js
@@ -1,31 +1,45 @@
-$(document).ready(function () {
-  console.log("ready!");
-});
+// $(document).ready(function () {
+//   console.log("ready!");
+// });
 
 // ** globals ** //
 var arrayOfClickedButtons = [];
+var total = [];
 
 // ** event handlers ** //
-$('span').not($('#cancel, #calc').on('click', function() {
+$('span').not($('.operator')).on('click', function() {
   var $buttonClicked = $(this).html();
   addToScreen($buttonClicked);
   arrayOfClickedButtons.push($buttonClicked);
-})
+});
 
-//clear button
+// ** handle clear button ** //
+$('.operator').not($('#cancel, #calc')).on('click', function(){
+  var $operatorClicked = $(this).html();
+  $('#screen').append($operatorClicked + '&nbsp;');
+  total.push(arrayOfClickedButtons.join(''));
+  total.push($operatorClicked);
+  arrayOfClickedButtons = [];
+  console.log(total);
+});
+
+// ** handle clear button ** //
 $('#cancel').on('click', function(){
   $('#screen').html('');
   arrayOfClickedButtons = [];
+  total = [];
 });
 
-// ** helper function ** //
-$(#calc).on('click',function addToScreen(btn) {
-  var answer = calculate(arrayOfClickedButtons);
+// ** handle equals button ** //
+$('#calc').on('click',function addToScreen(btn) {
+  total.push(arrayOfClickedButtons.join(''));
+  var answer = calculate(total);
   $('#screen').html(answer);
   arrayOfClickedButtons = [];
-}
+  total = [];
+});
 
-
+// ** helper functions ** //
 function addToScreen(btn) {
-  $('#screen').append(btn + '&nbsp;')
+  $('#screen').append(btn + '&nbsp;');
 }
